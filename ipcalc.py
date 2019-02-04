3732c926e3fc037ffa5716480747e54d592ebf8c
Naveenkota55 <naveen.kota@dal.ca>
2019-02-04 09:03:27 -0400

    ipcalc

---
+++ ipcalc.py
@@ -0,0 +1,12 @@
+from array import array
+import netadder
+
+def subnet_calculator():
+    class_A = ('10.0.0.1')
+    class_B = ('172.16.0.1')
+    class_C = ('192.168.0.1')
+
+
+    print ("CLASS A = 10.0.0.1")
+    print ("CLASS B = 172.16.0.1")
+    print ("CLASS C = 192.168.0.1")
